a87fffa945f49e4336ea65907355191a47029dde
pratikshah11 <pratikshah964@gmail.com>
2022-11-03 16:48:00 -0400

    in place operator, walrus operator

---
+++ lecture2.4.py
@@ -0,0 +1,18 @@
+#in place operators - helps to reduce the mathematical function like x=x+3 is equal to x+=3
+'''
+x=2
+print(x)
+x+=3
+print(x)
+'''
+
+#it can be also used in strings
+'''
+x = "Pratik"
+print(x)
+x+=" Shah"
+print(x)
+'''
+
+#walrus operator(:- ) - this operator reduces code of line(print and input line is made into one)
+print(int(input("What is your age?")))
