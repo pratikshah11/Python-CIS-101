17486f530e34f3d15a357f65619680af0265d04c
pratikshah11 <pratikshah964@gmail.com>
2022-11-03 16:49:47 -0400

    boolean, comparison operator

---
+++ lecture3.1.py
@@ -0,0 +1,18 @@
+#boolean -gives an output as True or False
+pratik = True
+print(pratik)
+
+print(2==3)
+print("Pratik"=="Pratik")
+
+#comparison operator- the operator != is a sign for not equal to which can be used in expressions
+print(1 != 1)
+print("pratik"!="shah")
+
+#comparision - operators such as >,<,<=,>= are comparison operators and are used for comparing 2 things and should be used for numbers
+print(4>=4)
+print(5<=3)
+
+#if comparision signs are used for alphabets then the computer will check for the alphabetical order
+print("pratik">="pratham")
+#the output will be true because the first 4 values in the name prat are same, but i comes after h in alphabetical order so, the statement is true
