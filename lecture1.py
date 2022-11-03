cd995b16a9a434633fdc1bce1c3461e34957e2f4
pratikshah11 <pratikshah964@gmail.com>
2022-11-03 16:38:25 -0400

    print operator, basic mathematical functions

---
+++ lecture1.py
@@ -0,0 +1,14 @@
+#printing a number
+print(100)
+
+#printing a statement
+print("hello world")
+
+#maths expressiom
+print(4+5)  #add
+print(4-5)  #subract
+print(4*5)  #multiply   
+print(4/5)  #divide 
+print(4**3) #exponents
+print(30//25)   #whole number division
+print(4%3)  #remainder
