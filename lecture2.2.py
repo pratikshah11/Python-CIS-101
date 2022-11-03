688c49f104fa02f0bce8b122cadcf739147bddbb
pratikshah11 <pratikshah964@gmail.com>
2022-11-03 16:44:10 -0400

    variables

---
+++ lecture2.2.py
@@ -0,0 +1,19 @@
+#variable - used to store data
+#variable names can only be numbers, letters, and underscore and they should not start with a number
+user_name= "Pratik"
+user_age=19
+print(user_name)
+print(user_age)
+
+#variables can be used in corresponding operations
+print(2022 - user_age)
+print(user_name *3)
+
+#variables can be reassigned values multiple times
+x =2000
+print(x)
+
+x= "LFC"
+print(x)
+
+
