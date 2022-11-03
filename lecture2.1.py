f3ecf86ca7d4e231a5958899d76f8f7d337f2d88
pratikshah11 <pratikshah964@gmail.com>
2022-11-03 16:40:56 -0400

    string, newline, concatenation operators

---
+++ lecture2.1.py
@@ -0,0 +1,21 @@
+#strings can be used by both single and double quotation(prefer using double quotes)
+print('hello world')
+print("hello world")
+
+#for using 's word and single quote, then use a backslash
+print('he\'s')
+
+#newline
+#adding text in a new line can be done by using \n
+print("one\ntwo\nthree")
+#using three double quotation can also be used for newlines
+print("""Hello
+World""")
+
+#Concatenation - it can be done with both integers and string
+#Its used to join two or more things in simple words
+print("Pratik" + "Shah")
+print("5"+"4")
+#strings can also be multiplied
+print("Pratik"*3)
+print("5"*4)
