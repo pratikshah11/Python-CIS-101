9ceffb3bae67425d33ff1edf98271ea9ff9d29d4
pratikshah11 <pratikshah964@gmail.com>
2022-11-03 16:46:27 -0400

    taking user input and concatenating that

---
+++ lecture2.3.py
@@ -0,0 +1,27 @@
+#taking input from user
+'''
+x= input()
+print(x)
+'''
+
+#taking input from user by displaying a message
+'''
+age= input("What is your age? ")
+print("Your age is " + age)
+'''
+
+#we can decide the type of variable we want to input from the user
+'''
+age= int(input("What is your age? "))
+print("Your age is " + str(age))
+'''
+
+#concatenating inputs
+age = int(input("What is your age? "))
+name = str(input("WHat is your name?"))
+print(name + " is " + str(age) + " years old.")
+
+
+
+
+
