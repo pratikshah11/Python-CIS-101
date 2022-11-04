2225c0325c1e7bd2eb048983e5518401c2751d22
pratikshah11 <pratikshah964@gmail.com>
2022-11-04 12:15:35 -0400

    list functions

---
+++ lecture3.5.py
@@ -0,0 +1,21 @@
+#list functions
+#append function- this method is used to one element to a list
+'''nums = [1, 2, 3]
+nums.append(4)
+print(nums)'''
+
+#len function- this function is used to count the number of elements in a list(but it will give one less no. because the count starts from 0 in lists)
+'''nums = [1,2,3,5,6,7]
+print(len(nums))'''
+
+#insert function - this method is used to add an element in the middle of a list
+'''nums=[1,2,4,5,6,7,8,9,0]
+index=2
+nums.insert(index,3)
+print(nums)'''
+
+#index method-this index method is used to find the occurance of the element in a list
+letters = [5,4,3,2,1]
+print(letters.index(3))
+print(letters.index(5))
+print(letters.index(1))
